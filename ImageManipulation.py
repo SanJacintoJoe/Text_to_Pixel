c20058205f4116f0680889521965928e16a1f4f7
SanJacintoJoe <61699075+SanJacintoJoe@users.noreply.github.com>
2020-10-15 18:22:24 -0400

    Update ImageManipulation.py

--- ImageManipulation.py
+++ ImageManipulation.py
@@ -6,6 +6,7 @@ def ToBinary(n):
         binary = bin(n).replace("0b", "")
     return binary
 
+
 def ToChar(binary):
     Output = 0
     size = len(binary)-1
@@ -13,12 +14,14 @@ def ToChar(binary):
         Output += int(x) * (2**(size-position))
     return Output
 
+
 def Message_Embed(binary, value_past):
     if int(binary[-1]) != value_past:
         binary = binary[:-1] + str(value_past)
     else:
         pass
     return binary
+
 
 def split_message_into_char(message):
     chars = []
